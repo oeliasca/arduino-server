9beb89e6c0d7be6ff8ef4cb15073049b9844a5ef
Oriol Elias <oeliasca@gmail.com>
2024-12-31 00:07:34 +0100

    url

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 import express from "express";
+import url from "url";
 
 const app = express();
+var test = 1;
 
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+//?ha=1&ht=2&t=3
+app.get("/arduino/dashboard", (req, res) => {
+  var params = url.parse(req.url, true).query;
+  res.send(`
+    <html>
+<body>
+<h1>Clima Rubí</h1>
+<p>humitat ambiental: ${params.ha}</p>
+<p>humitat terra: ${params.ht}</p>
+<p>temperatura: ${params.t}</p>
+</body>
+</html>
+    `);
 });
 
 app.listen(8080);
